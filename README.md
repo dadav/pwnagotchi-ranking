9d6f45718878a4de64b01c1315eb0a9747879818
dadav <33197631+dadav@users.noreply.github.com>
2019-10-19 13:06:11 +0200

    init

---
+++ README.md
@@ -0,0 +1,11 @@
+# usage
+```bash
+# top units sorted by captured networks
+./rank.py --max 10 --key networks
+
+# top units sorted by epochs
+./rank.py --max 10 --key data_epochs
+
+# top units sorted by deauths
+./rank.py --max 10 --key data_deauthed
+```
---
+++ rank.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python3
+
+import os
+from collections.abc import MutableMapping
+import argparse
+import requests
+
+
+API_ENDPOINT = "https://api.pwnagotchi.ai/api/v1/units"
+
+class Termcolors:
+    BG_GREEN = '\033[42m'
+    FG_BLACK = '\033[30m'
+    CLEAR = '\033[0m'
+
+class Unit(dict):
+    """
+    Unit wrapper
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(Unit, self).__init__(*args, **kwargs)
+        for arg in args:
+            if isinstance(arg, dict):
+                for key, value in arg.items():
+                    self[key] = value
+
+        if kwargs:
+            for key, value in kwargs.items():
+                self[key] = value
+
+    def __getattr__(self, attr):
+        return self.get(attr)
+
+    def __setattr__(self, key, value):
+        self.__setitem__(key, value)
+
+    def __setitem__(self, key, value):
+        super(Unit, self).__setitem__(key, value)
+        self.__dict__.update({key: value})
+
+    def __delattr__(self, item):
+        self.__delitem__(item)
+
+    def __delitem__(self, key):
+        super(Unit, self).__delitem__(key)
+        del self.__dict__[key]
+
+# https://stackoverflow.com/questions/6027558/flatten-nested-dictionaries-compressing-keys
+def _flatten(data, parent_key='', sep='_'):
+    items = list()
+    for key, value in data.items():
+        new_key = parent_key + sep + key if parent_key else key
+        if isinstance(value, MutableMapping):
+            items.extend(_flatten(value, new_key, sep=sep).items())
+        else:
+            items.append((new_key, value))
+    return dict(items)
+
+
+def _fetch_page(page=1):
+    try:
+        result = requests.get(f"{API_ENDPOINT}?p={page}")
+        return result.json()
+    except Exception as ex:
+        raise ex
+
+def get_all_units():
+    """
+    Fetches all units
+
+    returns list of Unit-class instances
+    """
+    page = 1
+    result = list()
+    while True:
+        try:
+            data = _fetch_page(page)
+            if not data['units']:
+                break
+            for unit in data['units']:
+                result.append(Unit(_flatten(unit)))
+            page += 1
+        except Exception as ex:
+            break
+
+    return result
+
+def _sort_units_by(units, sort_key='networks'):
+    return sorted(units, key=lambda x: x.get(sort_key, 0), reverse=True)
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Compare the top pwnagotchis!")
+    parser.add_argument('--max', type=int, default=10, help="Max number of units")
+    parser.add_argument('--key', default="networks", help="What key should be used for sorting")
+    args = parser.parse_args()
+
+    print("Fetching units...")
+    units = get_all_units()
+    top_units = _sort_units_by(units, sort_key=args.key)[:args.max]
+    _, term_columns = os.popen('stty size', 'r').read().split()
+    term_columns = int(term_columns) / 2
+    max_value = top_units[0][args.key]
+
+    for unit in top_units:
+        text = f"{unit.name}: {unit.get(args.key)}"
+        line_width = int(term_columns) / int(max_value) * int(unit.get(args.key))
+        padding_len = int(line_width) - len(text)
+        if padding_len > 0:
+            padding = " " * padding_len
+        else:
+            padding = ""
+
+        print(f"{Termcolors.BG_GREEN}{Termcolors.FG_BLACK}{unit.name}: {unit.get(args.key)}{padding}{Termcolors.CLEAR}")
+
+
+if __name__ == "__main__":
+    SystemExit(main())
+
