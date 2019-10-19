7e60cf421c263eb237691fccedc2417a1c969502
dadav <33197631+dadav@users.noreply.github.com>
2019-10-19 13:31:15 +0200

    update

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # usage
 ```bash
+# possible keys
+data_session_associated
+data_session_avg_reward
+data_session_deauthed
+data_session_epochs
+data_session_handshakes
+data_session_max_reward
+data_session_min_reward
+data_session_peers
+data_session_train_epochs
+
 # top units sorted by captured networks
 ./rank.py --max 10 --key networks
 
 # top units sorted by epochs
-./rank.py --max 10 --key data_advertisement_epoch
+./rank.py --max 10 --key data_session_epochs
 
 # top units sorted by deauths
-./rank.py --max 10 --key data_advertisement_pwnd_tot
+./rank.py --max 10 --key data_sessions_deauthed
 ```
 
 ## example
--- rank.py
+++ rank.py
@@ -65,7 +65,7 @@ def _fetch_page(page=1):
     except Exception as ex:
         raise ex
 
-def get_all_units():
+def get_units(max_cnt=None):
     """
     Fetches all units
 
@@ -80,6 +80,8 @@ def get_all_units():
                 break
             for unit in data['units']:
                 result.append(Unit(_flatten(unit)))
+                if max_cnt and len(result) >= max_cnt:
+                    return result
             page += 1
         except Exception as ex:
             break
@@ -89,15 +91,31 @@ def get_all_units():
 def _sort_units_by(units, sort_key='networks'):
     return sorted(units, key=lambda x: x.get(sort_key, 0), reverse=True)
 
+def is_number(s):
+    try:
+        complex(s) # for int, long, float and complex
+    except ValueError:
+        return False
+
+    return True
 
 def main():
     parser = argparse.ArgumentParser(description="Compare the top pwnagotchis!")
     parser.add_argument('--max', type=int, default=10, help="Max number of units")
     parser.add_argument('--key', default="networks", help="What key should be used for sorting")
+    parser.add_argument('--list', action="store_true", help="List the keys you could use for sorting")
     args = parser.parse_args()
 
     print("Fetching units...")
-    units = get_all_units()
+    if args.list:
+        units = get_units(max_cnt=1)
+        all_keys = units[0].keys()
+        for key in all_keys:
+            if is_number(units[0][key]):
+                print(key)
+        return 0
+
+    units = get_units()
     top_units = _sort_units_by(units, sort_key=args.key)[:args.max]
     _, term_columns = os.popen('stty size', 'r').read().split()
     term_columns = int(term_columns) / 2
